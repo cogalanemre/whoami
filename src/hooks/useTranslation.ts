00d95549e69284572f7a2ba964c356d3b09da85a
Emre COGALAN <comengcogalan@gmail.com>
2025-02-19 23:42:20 +0300

    v0.2.2

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -3,25 +3,13 @@
 import { useParams } from 'next/navigation';
 import trCommon from '@/locales/tr/common.json';
 import enCommon from '@/locales/en/common.json';
-import trPersonal from '@/locales/tr/personal.json';
-import enPersonal from '@/locales/en/personal.json';
-import trExperiences from '@/locales/tr/experiences.json';
-import enExperiences from '@/locales/en/experiences.json';
-import trEducation from '@/locales/tr/education.json';
-import enEducation from '@/locales/en/education.json';
 
 const translations = {
   tr: {
     common: trCommon,
-    personal: trPersonal,
-    experiences: trExperiences,
-    education: trEducation,
   },
   en: {
     common: enCommon,
-    personal: enPersonal,
-    experiences: enExperiences,
-    education: enEducation,
   },
 } as const;
 
