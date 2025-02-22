0c7250f133634ff5363f6fd34831c9a12abe737d
Emre COGALAN <comengcogalan@gmail.com>
2025-02-22 12:33:14 +0300

    v0.3.18

--- src/components/atoms/buttons/SocialMediaButton.tsx
+++ src/components/atoms/buttons/SocialMediaButton.tsx
@@ -54,6 +54,9 @@ export default function SocialMediaButton({
         border: "2px solid",
         borderColor: "primary.main",
         backdropFilter: "blur(4px)",
+        "& > svg": {
+          fontSize: "1.5rem",
+        },
         "&:hover": {
           transform: "translateY(-2px)",
           transition: "all 0.2s ease-in-out",
